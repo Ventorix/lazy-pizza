c28dfbf03ed8fee91506a611f95bbefbb4aa55ae
1Fenix7 <userpk66@gmail.com>
2023-09-29 06:12:39 +0300

    feat: add fetching order  data from project API

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
 	return (
 		<header>
 			<Link to={'/'}>Lazy Pizza</Link>
+
+			<SearchOrder />
+
 			<p>User</p>
 		</header>
 	);
