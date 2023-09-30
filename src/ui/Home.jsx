5cd7e8e0ce2581ac3d7d37391e5c0512aa71cc9b
1Fenix7 <userpk66@gmail.com>
2023-09-30 03:02:21 +0300

    build(tailwind): :heavy_plus_sign: add tailwind css for styling project

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
-	return (
-		<div>
-			<h1>
-				The lazy pizza.
-				<br />
-				Straight out of the oven, straight to you.
-			</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        The lazy pizza.
+        <br />
+        Straight out of the oven, straight to you.
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
