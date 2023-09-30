1f7b21fb841bc1dd62cb9bd23935c403bd6be55e
1Fenix7 <userpk66@gmail.com>
2023-10-01 01:29:09 +0300

    style: :lipstick: style a cart page

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,18 +1,21 @@
-import { formatCurrency } from '../../utils/helpers';
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartItem({ item }) {
-	const { pizzaId, name, quantity, totalPrice } = item;
+  const { pizzaId, name, quantity, totalPrice } = item;
 
-	return (
-		<li>
-			<p>
-				{quantity}&times; {name}
-			</p>
-			<div>
-				<p>{formatCurrency(totalPrice)}</p>
-			</div>
-		</li>
-	);
+  return (
+    <li className="xs:flex xs:items-center xs:justify-between py-3">
+      <p className="">
+        {quantity}&times; {name}
+      </p>
+      <div className="xs:gap-6 flex items-center justify-between">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+
+        <Button type={"small"}>Delete</Button>
+      </div>
+    </li>
+  );
 }
 
 export default CartItem;
