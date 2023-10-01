8ecf1e9b418a9f3c8ff91c04134b65889e1b70dc
1Fenix7 <userpk66@gmail.com>
2023-10-02 00:02:16 +0300

    chore: :lipstick: update the favicon and logo to a more suitable design

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b-2 border-stone-600 bg-indigo-600 px-4 py-3 uppercase text-stone-200 sm:px-6">
       <Link to={"/"} className="font-bold tracking-widest ">
-        Lazy Pizza
+        🍕Lazy Pizza
       </Link>
 
       <SearchOrder />
