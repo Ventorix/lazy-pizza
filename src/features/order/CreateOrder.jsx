a1c637f5993624cda92a78a662ae3ef1a61ffb32
1Fenix7 <userpk66@gmail.com>
2023-10-02 00:47:26 +0300

    feat: :sparkles: add feature to add new pizza item to cart array

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
-import Button from "../../ui/Button";
 import { useSelector } from "react-redux";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     "inline-block rounded-full text-sm  bg-indigo-600 font-semibold uppercase tracking-wide text-stone-200 transition-colors duration-300 hover:border-stone-900 hover:bg-indigo-800 focus:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-500 focus:ring-offset-1 active:bg-indigo-800 disabled:cursor-not-allowed";
 
@@ -17,6 +17,14 @@ function Button({ children, disabled, to, type }) {
         {children}
       </Link>
     );
+
+  if (onClick) {
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+  }
 
   return (
     <button disabled={disabled} className={styles[type]}>
